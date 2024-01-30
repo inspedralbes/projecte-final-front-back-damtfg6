ff83b4b1d83b20c36a42f917299e06537d5ac240
C0ural1 <albertcoralm@gmail.com>
2024-01-30 11:43:01 +0100

    canvis inicials server

--- M09 - Server/server.js
+++ M09 - Server/server.js
@@ -1,17 +1,20 @@
-const mysql = require('mysql2/promise');
+// Importació de mòduls
 const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-var cors = require("cors");
-const socketIO = require('socket.io');
-//const { spawn } = require('child_process');
-//const path = require('path');
-var session = require('express-session')
-const PORT = 3788;
 const http = require('http');
+const socketIO = require('socket.io');
+const mysql = require('mysql2/promise');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const session = require('express-session');
+
+// Configuració de l'aplicació Express
+const app = express();
+const PORT = 3788;
+
+// Creació del servidor HTTP utilitzant Express
 const httpServer = http.createServer(app);
 
-
+// Configuració de Socket.IO 
 const io = socketIO(httpServer, {
     cors: {
         origin: "*",
@@ -19,23 +22,54 @@ const io = socketIO(httpServer, {
     }
 });
 
+// Configuració de CORS 
 app.use(cors({
     origin: function (origin, callback) {
         return callback(null, true);
     }
 }));
 
+// Configuració de la sessió d'Express
 app.use(session({
     secret: 'keyboardcat',
     resave: false,
     saveUninitialized: true,
     cookie: { secure: true }
-}))
+}));
 
-app.use(bodyParser.urlencoded({ extended: false }))
+// Configuració de body-parser
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Inici del servidor HTTP
 httpServer.listen(PORT, () => {
-    console.log("Server => " + PORT);
+    console.log("Servidor => " + PORT);
 });
+
+var history = require('connect-history-api-fallback')
+const staticFileMiddleware = express.static('../dist');
+app.use(staticFileMiddleware);
+app.use(history({
+    disableDotRule: true,
+    verbose: true
+}));
+app.use(staticFileMiddleware);
