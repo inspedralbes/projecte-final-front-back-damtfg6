b2aa4f2f102506de39069e8b666b6f2af8c1ea10
C0ural1 <albertcoralm@gmail.com>
2024-02-09 13:22:18 +0100

    Mil canvis

--- M09 - Server/scriptSQL.js
+++ M09 - Server/scriptSQL.js
@@ -1,5 +1,5 @@
 const mysql = require('mysql2/promise');
-module.exports = { registrarUsuari, registrarTutor, getUsuarisLoginAndroid};
+module.exports = { registrarUsuari, registrarTutor, getUsuarisLoginAndroid, registrarTutoritzacio, verificarUsuario};
 
 const connection = mysql.createPool({
     host: "dam.inspedralbes.cat",
@@ -13,9 +13,9 @@ const connection = mysql.createPool({
 async function getUsuarisLoginAndroid(connection) {
     try {
         const [rows, fields] = await connection.execute(`
-            SELECT nomCognoms, dni, telefon, correu, contrasenya, usuari_identificador, 'usuari' as rol FROM Usuaris
+            SELECT user_id, nomCognoms, dni, telefon, correu, contrasenya, usuari_identificador, 'usuari' as rol FROM Usuaris
             UNION
-            SELECT nomCognoms, dni, telefon, correu, contrasenya, usuari_identificador, 'tutor' as rol FROM Familiar
+            SELECT familiar_id, nomCognoms, dni, telefon, correu, contrasenya, usuari_identificador, 'tutor' as rol FROM Familiar
         `);
         const usuariosJSON = JSON.stringify(rows);
         return usuariosJSON;
@@ -50,11 +50,11 @@ async function registrarUsuari(connection, usuari) {
 
 async function registrarTutor(connection, familiar) {
     try {
-        const { nomCognoms, dni, telefon, correu, contrasenya, usuari_identificador } = familiar;
+        const { nomCognoms, dni, telefon, correu, contrasenya } = familiar;
 
         const [result] = await connection.execute(
-            'INSERT INTO Familiar (nomCognoms, dni, telefon, correu, contrasenya, usuari_identificador) VALUES (?, ?, ?, ?, ?, ?)',
-            [nomCognoms, dni, telefon, correu, contrasenya, usuari_identificador]
+            'INSERT INTO Familiar (nomCognoms, dni, telefon, correu, contrasenya) VALUES (?, ?, ?, ?, ?)',
+            [nomCognoms, dni, telefon, correu, contrasenya]
         );
         
         if (result.affectedRows === 1) {
@@ -69,4 +69,47 @@ async function registrarTutor(connection, familiar) {
         return false;
     }
 }
+
+
+async function registrarTutoritzacio(connection, familiarId, usuariIdentificador) {
+    try {
+
+        console.log("Familiar ID 1: ", familiarId);
+        
+        const familiar_id = familiarId;
+        const usuari_identificador = usuariIdentificador;
+
+        console.log("Familiar ID 2: ", familiar_id);
+
+        const [result] = await connection.execute(
+            'UPDATE Familiar SET usuari_identificador = ? WHERE familiar_id = ?', [usuari_identificador, familiar_id]
+        );
+
+        if (result.affectedRows === 1) {
+            console.log("Familiar actualizado correctamente");
+            return true;
+        } else {
+            console.log("Error al actualizar el familiar en la base de datos");
+            return false;
+        }
+    } catch (error) {
+        console.error('Error al actualizar el familiar:', error.message);
+        return false;
+    }
+}
+
+async function verificarUsuario(connection, usuariIdentificador) {
+    try {
+        const [rows] = await connection.execute(
+            'SELECT COUNT(*) AS count FROM Usuaris WHERE usuari_identificador = ?', [usuariIdentificador]
+        );
+        const count = rows[0].count;
+        return count > 0;
+    } catch (error) {
+        console.error('Error al verificar usuario:', error.message);
+        return false;
+    }
+}
+
+
 
--- M09 - Server/server.js
+++ M09 - Server/server.js
@@ -5,7 +5,7 @@ const mysql = require('mysql2/promise');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const session = require('express-session');
-const {registrarUsuari, getUsuarisLoginAndroid, registrarTutor} = require('./scriptSQL.js');
+const { registrarUsuari, getUsuarisLoginAndroid, registrarTutor, registrarTutoritzacio, verificarUsuario } = require('./scriptSQL.js');
 const app = express();
 const PORT = 3672;
 
@@ -16,7 +16,7 @@ httpServer.listen(PORT, () => {
 
 app.use(cors({
   origin: function (origin, callback) {
-      return callback(null, true);
+    return callback(null, true);
   }
 }));
 app.use(session({
@@ -38,8 +38,8 @@ const connection = mysql.createPool({
 //----------------------------------- CHAT -----------------------------------//
 const io = socketIO(httpServer, {
   cors: {
-      origin: "*",
-      methods: ["GET", "POST"]
+    origin: "*",
+    methods: ["GET", "POST"]
   }
 });
 io.on('connection', (socket) => {
@@ -47,96 +47,196 @@ io.on('connection', (socket) => {
 
   // Escuchar eventos específicos del cliente
   socket.on('chat message', (data) => {
-      console.log('message: ' + data.message);
-      console.log('sender: ' + data.nomCognoms);
+    console.log('message: ' + data.message);
+    console.log('sender: ' + data.nomCognoms);
 
-      // Emitir el mensaje a todos los usuarios
-      io.emit('chat message', data);
+    // Emitir el mensaje a todos los usuarios
+    io.emit('chat message', data);
 
   });
 
   socket.on('disconnect', () => {
-      console.log('user disconnected');
+    console.log('user disconnected');
   });
 });
 //----------------------------------- Usuaris Reigister i Login -----------------------------------//
 
 app.post('/registrarUsuari', async (req, res) => {
-    try {
-      const newUser = req.body;
-      const registrationResult = await registrarUsuari(connection, newUser);
-      if (registrationResult) {
-        res.json({ autoritzacio: true, message: 'Usuario registrado exitosamente' });
-      } else {
-        res.status(500).json({ autoritzacio: false, message: 'Error al registrar usuario' });
-        console.log("Error al registrar usuario")
-      }
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({ autoritzacio: false, message: 'Error en el servidor' });
-      console.log("Error en el servidor")
+  try {
+    const newUser = req.body;
+    const registrationResult = await registrarUsuari(connection, newUser);
+    if (registrationResult) {
+      res.json({ autoritzacio: true, message: 'Usuario registrado exitosamente' });
+    } else {
+      res.status(500).json({ autoritzacio: false, message: 'Error al registrar usuario' });
+      console.log("Error al registrar usuario")
     }
-  });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ autoritzacio: false, message: 'Error en el servidor' });
+    console.log("Error en el servidor")
+  }
+});
 
-  app.post('/registrarTutor', async (req, res) => {
-    try {
-      const newFamiliar = req.body;
-      const registrationResult = await registrarTutor(connection, newFamiliar);
-      if (registrationResult) {
-        res.json({ autoritzacio: true, message: 'Familiar registrado exitosamente' });
-      } else {
-        res.status(500).json({ autoritzacio: false, message: 'Error al registrar Familiar' });
-        console.log("Error al registrar Familiar")
-      }
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({ autoritzacio: false, message: 'Error en el servidor' });
-      console.log("Error en el servidor")
+app.post('/registrarTutor', async (req, res) => {
+  try {
+    const newFamiliar = req.body;
+    const registrationResult = await registrarTutor(connection, newFamiliar);
+    if (registrationResult) {
+      res.json({ autoritzacio: true, message: 'Familiar registrado exitosamente' });
+    } else {
+      res.status(500).json({ autoritzacio: false, message: 'Error al registrar Familiar' });
+      console.log("Error al registrar Familiar")
     }
-  });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ autoritzacio: false, message: 'Error en el servidor' });
+    console.log("Error en el servidor")
+  }
+});
 
-  app.post("/usuarisLogin", function (req, res) {
-    const user = req.body;
-    let autoritzacio = { "autoritzacio": false, "rol": 'usuari', "userData": null, "usuariTutoritzatData": null };
+//--------------------------------Nova Tutoritzacio------------------------------------//
+/*app.post('/registrarTutoritzacio', async (req, res) => {
+  try {
+    const familiarID = req.query.familiarID;
+    const identificador = req.query.identificador;
 
-    // Obtener usuarios de las tablas Usuaris y Familiar
-    getUsuarisLoginAndroid(connection).then((usuaris) => {
-        usuaris = JSON.parse(usuaris);
+    console.log("Familiar: ", familiarID);
+    console.log("Usuari Identificador: ", identificador);
 
-        // Buscar en la lista combinada de usuarios
-        for (let i = 0; i < usuaris.length && !autoritzacio.autoritzacio; i++) {
-            if (usuaris[i].dni == user.dni && usuaris[i].contrasenya == user.contrasenya) {
-                autoritzacio.autoritzacio = true;
-                autoritzacio.userData = usuaris[i];
-                autoritzacio.rol = usuaris[i].rol === "usuari" ? "usuari" : (usuaris[i].usuari_identificador ? "tutor" : "usuari");
 
-                // Buscar el usuario tutorizado si el usuario actual es un "Tutor"
-                if (autoritzacio.rol === "tutor") {
-                    for (let j = 0; j < usuaris.length; j++) {
-                        if (usuaris[i].usuari_identificador === usuaris[j].usuari_identificador && usuaris[j].rol === "usuari") {
-                            autoritzacio.usuariTutoritzatData = usuaris[j];
-                            break; // Terminar la búsqueda una vez que se haya encontrado el usuario tutorizado
-                        }
-                    }
-                }
 
-                req.session.nombre = user.nomUsuari;
+    // Verificar si existe un usuario con el identificador proporcionado
+    const usuarioExistente = await verificarUsuario(connection, identificador);
+
+    if (!usuarioExistente) {
+      console.log("El usuario con identificador ", identificador, " no existe.");
+      return res.json({ autoritzacio: false, message: 'Error usuari no existeix' });
+    }
+
+    // Proceder con la actualización del familiar en la base de datos
+    const registrationResult = await registrarTutoritzacio(connection, familiarID, identificador);
+    if (registrationResult) {
+      res.json({ autoritzacio: true, message: 'Familiar registrado exitosamente' });
+    } else {
+      res.status(500).json({ autoritzacio: false, message: 'Error al registrar Familiar' });
+      console.log("Error al registrar Familiar");
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ autoritzacio: false, message: 'Error en el servidor' });
+    console.log("Error en el servidor");
+  }
+});*/
+
+
+//----------------------------------- TUTO 2 -----------------------------------//
+
+
+app.post('/registrarTutoritzacio', async (req, res) => {
+  try {
+    let autoritzacio = { "autoritzacio": false, "usuariTutoritzatData": null };
+
+    const usuaris = JSON.parse(await getUsuarisLoginAndroid(connection));
+
+    const familiarID = req.query.familiarID;
+    const identificador = req.query.identificador;
+
+    console.log("Familiar: ", familiarID);
+    console.log("Usuari Identificador: ", identificador);
+
+    // Verificar si existe un usuario con el identificador proporcionado
+    const usuarioExistente = await verificarUsuario(connection, identificador);
+
+    if (!usuarioExistente) {
+      console.log("El usuario con identificador ", identificador, " no existe.");
+      return res.json({ autoritzacio: false, message: 'Error usuari no existeix' });
+    }
+
+    // Proceder con la actualización del familiar en la base de datos
+    const registrationResult = await registrarTutoritzacio(connection, familiarID, identificador);
+  
+
+    if (registrationResult) {
+      // Verificar si el usuario tiene el rol "usuari"
+      autoritzacio.autoritzacio = true;
+          for (let i = 0; i < usuaris.length; i++) {
+          let identificador2 = parseInt(identificador);
+          if (usuaris[i].rol === "usuari" && usuaris[i].usuari_identificador === identificador2) {
+              autoritzacio.usuariTutoritzatData = usuaris[i];
+              break; 
+          }
+      }
+  
+      console.log("Autoritzacio Tutoritzacio: ", autoritzacio);
+      res.json(autoritzacio);
+    } else {
+      res.status(500).json({ autoritzacio: false, message: 'Error al registrar Familiar' });
+      console.log("Error al registrar Familiar");
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ autoritzacio: false, message: 'Error en el servidor' });
+    console.log("Error en el servidor");
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+//-----------------------------------  usuaris Login -----------------------------------//
+
+app.post("/usuarisLogin", function (req, res) {
+  const user = req.body;
+  let autoritzacio = { "autoritzacio": false, "rol": 'usuari', "userData": null, "usuariTutoritzatData": null };
+
+  // Obtener usuarios de las tablas Usuaris y Familiar
+  getUsuarisLoginAndroid(connection).then((usuaris) => {
+    usuaris = JSON.parse(usuaris);
+
+    // Buscar a la llista d'usuaris si el usuari i contrasenya coincideixen
+    for (let i = 0; i < usuaris.length && !autoritzacio.autoritzacio; i++) {
+      if (usuaris[i].dni == user.dni && usuaris[i].contrasenya == user.contrasenya) {
+        autoritzacio.autoritzacio = true;
+        autoritzacio.userData = usuaris[i];
+        //Trobar tutor
+        autoritzacio.rol = usuaris[i].rol === "tutor" ? "tutor" : "usuari";
+
+        // Buscar el usuario tutorizado si el usuario actual es un "Tutor"
+        if (autoritzacio.rol === "tutor") {
+          for (let j = 0; j < usuaris.length; j++) {
+            if (usuaris[i].usuari_identificador === usuaris[j].usuari_identificador && usuaris[j].rol === "usuari") {
+              autoritzacio.usuariTutoritzatData = usuaris[j];
+              break;
             }
+          }
         }
+        else {
+          autoritzacio.usuariTutoritzatData = {};
+        }
+      }
+    }
 
-        console.log("Autoritzacio Login: ", autoritzacio);
-        res.json(autoritzacio);
-    }).catch((error) => {
-        console.error('Error al obtener usuarios:', error.message);
-        res.status(500).json({ "error": "Error al obtener usuarios" });
-    });
+    console.log("Autoritzacio Login: ", autoritzacio);
+    res.json(autoritzacio);
+  }).catch((error) => {
+    console.error('Error al obtener usuarios:', error.message);
+    res.status(500).json({ "error": "Error al obtener usuarios" });
+  });
 });
 
 var history = require('connect-history-api-fallback')
 const staticFileMiddleware = express.static('../dist');
 app.use(staticFileMiddleware);
 app.use(history({
-    disableDotRule: true,
-    verbose: true
+  disableDotRule: true,
+  verbose: true
 }));
 app.use(staticFileMiddleware);
